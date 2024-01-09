a2cd0ca12da1d26c65b2e9f82a48e41cad402e2f
CaCtUs1k <denischernish19012004@gmail.com>
2024-01-09 14:50:00 +0200

    provide models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class WeatherReport(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
+    city = models.CharField(max_length=63)
+    temperature = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
