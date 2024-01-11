2fdaf6938732848d388c589eb81e9b2444585112
CaCtUs1k <denischernish19012004@gmail.com>
2024-01-11 13:08:01 +0200

    change urlpattern`s name

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ app_name = 'main'
 urlpatterns = [
     path('current_weather/', get_current_weather, name='current-weather'),
     path('weekly_weather/', get_weekly_weather, name='weekly-weather'),
-    path('hourly_weather/', get_hourly_weather, name='get-hourly-weather'),
+    path('hourly_weather/', get_hourly_weather, name='hourly-weather'),
 ]
